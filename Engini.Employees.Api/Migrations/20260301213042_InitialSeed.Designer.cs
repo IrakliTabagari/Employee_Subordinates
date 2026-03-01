35ba6500a0dae010f90231847a9b04a263424eaf
Irakli.Tabagari <iraklitabag89@gmail.com>
2026-03-02 01:31:41 +0400

    Initial_Seed

--- Engini.Employees.Api/Migrations/20260301213042_InitialSeed.Designer.cs
+++ Engini.Employees.Api/Migrations/20260301213042_InitialSeed.Designer.cs
@@ -3,6 +3,7 @@ using Engini.Employees.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Engini.Employees.Api.Migrations
 {
     [DbContext(typeof(EmployeesDbContext))]
-    partial class EmployeesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260301213042_InitialSeed")]
+    partial class InitialSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Engini.Employees.Api/Migrations/20260301213042_InitialSeed.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Engini.Employees.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "Employees",
+                columns: new[] { "Id", "ManagerId", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, "Alice (CEO)" },
+                    { 2, 1, "Bob (Head of Engineering)" },
+                    { 8, 1, "Henry (Head of HR)" },
+                    { 3, 2, "Carol (Engineering Manager)" },
+                    { 9, 8, "Irene (Recruiter)" },
+                    { 4, 3, "Dan (Team Lead)" },
+                    { 7, 3, "Grace (QA Engineer)" },
+                    { 5, 4, "Eva (Senior Developer)" },
+                    { 6, 4, "Frank (Developer)" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
